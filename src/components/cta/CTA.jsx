5923a74b7e316f952b3ef0513fb43d137baf98b9
mainz1011 <mainz1011@hotmail.com>
2023-05-31 23:54:36 +0300

    Add files via upload

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './cta.css'; 
+
+const CTA = () => (
+    <div className="gui_cta">
+      <div className="gui_cta-advocate">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today &amp; start exploring PandaTech.</h3>
+      </div>
+      <div className="gui_cta-button">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+
+export default CTA
